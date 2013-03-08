64bb0658bf382e79a6b71489b3fa1a14cbb97bf3
Maxime Biais <maxime.biais@gmail.com>
2013-03-08 12:09:34 +0100

    trader simulator

--- src/observers/traderbot.py
+++ src/observers/traderbot.py
@@ -1,5 +1,6 @@
 import logging
 import config
+import time
 from observer import Observer
 from private_markets import mtgox
 from private_markets import bitcoincentral
@@ -14,10 +15,14 @@ class TraderBot(Observer):
             "BitcoinCentralEUR": self.btcentral,
             "BitcoinCentralUSD": self.btcentral
             }
-        print self.mtgox
-        print self.btcentral
+        self.profit_thresh = 5 # in EUR
+        self.perc_thresh = 2 # in %
+        self.trade_wait = 120 # in seconds
+        self.last_trade = 0
 
-    def opportunity(self, profit, volume, buyprice, kask, sellprice, kbid, perc):
+    def opportunity(self, profit, volume, buyprice, kask, sellprice, kbid, perc, weighted_buyprice, weighted_sellprice):
+        if profit < self.profit_thresh or perc < self.perc_thresh:
+            return
         if kask not in self.clients:
             logging.warn("Can't automate this trade, client not available: %s" % (kask))
         if kbid not in self.clients:
@@ -36,9 +41,19 @@ class TraderBot(Observer):
                          self.clients[kbid].btc_balance))
             return
 
+        current_time = time.time()
+        if current_time - self.last_trade < self.trade_wait:
+            logging.warn("Can't automate this trade, last trade occured %s seconds ago" % (current_time - self.last_trade))
+            return
+
         # Everything's ok, ask for transactions
+        logging.info("Trader bot execute a transaction - profit: %f and perc: %f" % (profit, perc))
+        self.execute_trade(volume, kask, kbid, weighted_buyprice, weighted_sellprice)
+
+    def execute_trade(self, volume, kask, kbid, weighted_buyprice, weighted_sellprice):
         self.clients[kask].buy(volume)
         self.clients[kbid].sell(volume)
+
 
 
 
