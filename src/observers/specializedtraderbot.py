979dc78f2a618ce693ebce6bac2e372607274d47
Maxime Biais <maxime.biais@gmail.com>
2013-03-20 16:38:06 +0100

    catch URLError and HTTPError from markets HTTP GET

--- src/observers/specializedtraderbot.py
+++ src/observers/specializedtraderbot.py
@@ -16,7 +16,7 @@ class SpecializedTraderBot(Observer):
             "BitcoinCentralEUR": self.btcentral,
         }
         self.profit_thresholds = {  # Graph
-            "MtGoxEUR": {"BitcoinCentralEUR": 10},
+            "MtGoxEUR": {"BitcoinCentralEUR": 15},
             "BitcoinCentralEUR": {"MtGoxEUR": 5},
         }
         self.trade_wait = 60 * 5  # in seconds
@@ -34,15 +34,16 @@ class SpecializedTraderBot(Observer):
         self.execute_trade(*self.potential_trades[0][1:])
 
     def get_min_tradeable_volume(self, buyprice, eur_bal, btc_bal):
-        min1 = float(eur_bal) / ((1 + config.balance_margin) * buyprice)
-        min2 = float(btc_bal) / (1 + config.balance_margin)
+        min1 = float(eur_bal) / ((1. + config.balance_margin) * buyprice)
+        min2 = float(btc_bal) / (1. + config.balance_margin)
         return min(min1, min2)
 
     def update_balance(self):
         for kclient in self.clients:
             self.clients[kclient].get_info()
 
-    def opportunity(self, profit, volume, buyprice, kask, sellprice, kbid, perc, weighted_buyprice, weighted_sellprice):
+    def opportunity(self, profit, volume, buyprice, kask, sellprice, kbid, perc, weighted_buyprice,
+                    weighted_sellprice):
         if kask not in self.clients:
             logging.warn("Can't automate this trade, client not available: %s" % (kask))
             return
@@ -61,10 +62,11 @@ class SpecializedTraderBot(Observer):
         max_volume = self.get_min_tradeable_volume(buyprice, self.clients[kask].eur_balance,
                                                    self.clients[kbid].btc_balance)
         volume = min(volume, max_volume, config.max_tx_volume)
-
         if volume < config.min_tx_volume:
             logging.warn("Can't automate this trade, minimum volume transaction not reached %f/%f"
                          % (volume, config.min_tx_volume))
+            logging.info("Balance on %s: %f EUR - Balance on %s: %f BTC"(kask, self.clients[kask].eur_balance,
+                                                                         kbid, self.clients[kbid].btc_balance))
             return
 
         current_time = time.time()
