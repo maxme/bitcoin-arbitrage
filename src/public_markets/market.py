61da2c715ce6b339949e61ecec91b1ba0f33ddf8
Maxime Biais <maxime.biais@gmail.com>
2013-03-20 16:58:19 +0100

    fix typo

--- src/public_markets/market.py
+++ src/public_markets/market.py
@@ -34,7 +34,7 @@ class Market(object):
     def get_ticker(self):
         depth = self.get_depth()
         res = {'ask': 0, 'bid': 0}
-        if (len(depth['asks']) > 0 and len(depth["bids"]) > 0:
+        if len(depth['asks']) > 0 and len(depth["bids"]) > 0:
             res = {'ask': depth['asks'][0],
                    'bid': depth['bids'][0]}
         return res
