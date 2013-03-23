aece35f6995f108d35eb4b175bcf25fb9be52d2a
Benoit HERVIER <khertan@khertan.net>
2013-03-23 16:56:36 +0100

    try/except urllib error

--- src/public_markets/mtgoxeur.py
+++ src/public_markets/mtgoxeur.py
@@ -10,16 +10,19 @@ class MtGoxEUR(Market):
         self.depth = {'asks': [{'price': 0, 'amount': 0}], 'bids': [{'price': 0, 'amount': 0}]}
 
     def update_depth(self):
-        res = urllib2.urlopen('http://data.mtgox.com/api/2/BTCEUR/money/depth')
-        jsonstr = res.read()
-        try:
-            data = json.loads(jsonstr)
-        except Exception:
-            logging.error("%s - Can't parse json: %s" % (self.name, jsonstr))
-        if data["result"] == "success":
-            self.depth = self.format_depth(data["data"])
-        else:
-            logging.error("%s - fetched data error" % (self.name))
+	try:
+            res = urllib2.urlopen('http://data.mtgox.com/api/2/BTCEUR/money/depth')
+            jsonstr = res.read()
+            try:
+                data = json.loads(jsonstr)
+            except Exception:
+                logging.error("%s - Can't parse json: %s" % (self.name, jsonstr))
+            if data["result"] == "success":
+                self.depth = self.format_depth(data["data"])
+            else:
+                logging.error("%s - fetched data error" % (self.name))
+	except Exception:
+	    logging.error("%s - can't fetch data error" % (self.name))
 
     def sort_and_format(self, l, reverse=False):
         l.sort(key=lambda x: float(x["price"]), reverse=reverse)
