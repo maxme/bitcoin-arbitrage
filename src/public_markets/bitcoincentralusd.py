6dd4024fdf6a03f35e6c4acd2f9baa52faa4c1ff
Carl Fyffe <carl.fyffe@gmail.com>
2013-04-13 21:59:00 -0300

    Create bitcoincentralusd.py

---
+++ src/public_markets/bitcoincentralusd.py
@@ -0,0 +1,32 @@
+import urllib2
+import json
+from market import Market
+
+
+class BitcoinCentralUSD(Market):
+    def __init__(self):
+        super(BitcoinCentralUSD, self).__init__("USD")
+        # bitcoin central maximum call / day = 5000
+        # keep 2500 for other operations
+        self.update_rate = 24 * 60 * 60 / 2500
+
+    def update_depth(self):
+        res = urllib2.urlopen('https://bitcoin-central.net/api/v1/depth?currency=USD')
+        depth = json.loads(res.read())
+        self.depth = self.format_depth(depth)
+
+    def sort_and_format(self, l, reverse=False):
+        l.sort(key=lambda x: float(x['price']), reverse=reverse)
+        r = []
+        for i in l:
+            r.append({'price': float(i['price']), 'amount': float(i['amount'])})
+        return r
+
+    def format_depth(self, depth):
+        bids = self.sort_and_format(depth['bids'], True)
+        asks = self.sort_and_format(depth['asks'], False)
+        return {'asks': asks, 'bids': bids}
+
+if __name__ == "__main__":
+    market = BitcoinCentralUSD()
+    print market.get_ticker()
