53202ee663f9fd1618cdb7f8e97bcf56e2e5df51
mdespriee <matdpro@gmail.com>
2013-08-26 22:53:01 -0300

    force flush on logger, to avoid buffering

--- arbitrage/observers/logger.py
+++ arbitrage/observers/logger.py
@@ -5,4 +5,6 @@ from .observer import Observer
 class Logger(Observer):
     def opportunity(self, profit, volume, buyprice, kask, sellprice, kbid, perc,
                     weighted_buyprice, weighted_sellprice):
-        logging.info("profit: %f USD with volume: %f BTC - buy at %.4f (%s) sell at %.4f (%s) ~%.2f%%" % (profit, volume, buyprice, kask, sellprice, kbid, perc))
+        logging.info("profit: %f USD with volume: %f BTC - buy at %.4f (%s) sell at %.4f (%s) ~%.2f%%" % (
+        profit, volume, buyprice, kask, sellprice, kbid, perc))
+        [h.flush() for h in logging.getLogger().handlers]
