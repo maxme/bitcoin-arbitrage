ccfe8d6230f453d0c52435c2f380459d9df82ba9
Ryan Casey <ryepdx@gmail.com>
2013-10-29 10:30:06 -0700

    Finished work on the Btce private market. Verified functionality with some manual testing.

--- arbitrage/private_markets/btce.py
+++ arbitrage/private_markets/btce.py
@@ -4,7 +4,7 @@ from .market import Market, TradeException
 import time
 import base64
 import hmac
-import urllib.urlencode
+import urllib.parse
 import http.client
 import hashlib
 import sys
@@ -13,22 +13,44 @@ import config
 import random
 import logging
 
+class FileKV(object):
+    def set(self, key, value):
+        with open(key, 'w') as f:
+            f.write(value)
+
+    def get(self, key, default = None):
+        try:
+            with open(key, 'r') as f:
+                result = f.read()
+        except OSError:
+            result = default
+
+        return result
+
 class PrivateBtce(Market):
 
-    def __init__(self):
+    def __init__(self, kv_store=FileKV()):
         super().__init__()
         self.api_key = config.btce_api_key
         self.api_secret = config.btce_api_secret
+        self.kv = kv_store
+        self.nonce = self._get_nonce()
         self.get_info()
 
-    def _send_request(self, params, extra_headers=None):
-        params.update({
-            "nonce": random.SystemRandom().randint(sys.maxsize*-1, sys.maxsize)
-        })
-        params = urllib.urlencode(params)
+    def _get_nonce(self):
+        return int(self.kv.get("%s.nonce" % self.api_key, default = 0))
 
-        hash = hmac.new(self.api_secret, digestmod=hashlib.sha512)
-        hash.update(params)
+    def _send_request(self, params, extra_headers=None):
+        self.nonce += 1
+        params.update({
+            "nonce": self.nonce
+        })
+        params = urllib.parse.urlencode(params)
+
+        hash = hmac.new(
+            bytes(self.api_secret, 'utf-8'), digestmod=hashlib.sha512
+        )
+        hash.update(bytes(params, 'utf-8'))
 
         headers = {
             'Content-type': 'application/x-www-form-urlencoded',
@@ -43,19 +65,35 @@ class PrivateBtce(Market):
         conn = http.client.HTTPSConnection("btc-e.com")
         conn.request("POST", "/tapi", params, headers)
         response = conn.getresponse()
+        self.kv.set("%s.nonce" % self.api_key, str(self.nonce))
 
-        if reponse.status == 200:
-            return json.load(response)
+        if response.status == 200:
+            return json.loads(response.readall().decode('utf-8'))
         return None
 
     def execute(self, trade):
         logging.debug("[BTC-e] Got command \"%s\"" % str(trade))
+        pair = "%s_%s" % (
+            trade.amount_currency.lower(), trade.price_currency.lower()
+        )
+        response = self._try_execute(trade, pair)
+
+        if response["success"] == 0 \
+        and response["error"] == 'invalid parameter: pair':
+            pair = "%s_%s" % (
+                trade.price_currency.lower(), trade.amount_currency.lower()
+            )
+            response = self._try_execute(trade, pair)
+
+        return response
+
+    def _try_execute(self, trade, pair): 
         return self._send_request({
             "method": "Trade",
             "type": trade.type,
             "rate": trade.price,
             "amount": trade.amount,
-            "pair": "%s_%s" % (trade.amount_currency, trade.price_currency)
+            "pair": pair
         })
 
     def get_info(self):
@@ -63,6 +101,7 @@ class PrivateBtce(Market):
         response = self._send_request({
             "method": "getInfo"
         })
+
         if response and "return" in response and "funds" in response["return"]:
-            for currency, balance in response["return"]["funds"]:
+            for currency, balance in response["return"]["funds"].items():
                 self.balances[currency.upper()] = float(balance)
