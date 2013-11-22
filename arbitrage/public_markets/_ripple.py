cff371b862d7dcc6b095045ff813f3daca92747c
Markus Teufelberger <markusteufelberger@gmail.com>
2013-11-22 13:29:58 +0100

    pep8ified Ripple public markets

--- arbitrage/public_markets/_ripple.py
+++ arbitrage/public_markets/_ripple.py
@@ -5,31 +5,52 @@ import json
 from .market import Market
 import config
 
+
 class Ripple(Market):
     def __init__(self, currency, code):
         super().__init__(currency)
-        self.code = code  # the counter currency issuer to BTC, e.g. "rvYAfWj5gh67oV6fW32ZzP3Aw4Eubs59B" (Bitstamp)
+        self.code = code  # the counter currency issuer to BTC
         self.update_rate = 20
 
     def update_depth(self):
         url = config.ripple_rippled
-        biddata = '{ "method" : "book_offers", "params" : [ { "taker_pays" : { "currency" : "BTC", "issuer" : "' + config.ripple_BTC_issuer + '" }, "taker_gets" : { "currency" : "' + self.currency + '", "issuer" : "' + self.code + '" } } ] }'
+        biddata = ('{ "method" : "book_offers", '
+                   '"params" : [ { "taker_pays" : '
+                   '{ "currency" : "BTC", "issuer" : "' +
+                   config.ripple_BTC_issuer +
+                   '" }, "taker_gets" : '
+                   '{ "currency" : "' +
+                   self.currency +
+                   '", "issuer" : "' +
+                   self.code + '" } } ] }')
         biddata = biddata.encode('ascii')
-        bidreq = urllib.request.Request(url, biddata,
-                                     headers={
-            "Content-Type": "application/x-www-form-urlencoded",
-            "Accept": "*/*",
-            "User-Agent": "curl/7.24.0 (x86_64-apple-darwin12.0)"})
+        bidreq = urllib.request.Request(
+            url,
+            biddata,
+            headers={
+                "Content-Type": "application/x-www-form-urlencoded",
+                "Accept": "*/*",
+                "User-Agent": "curl/7.24.0 (x86_64-apple-darwin12.0)"})
         bidres = urllib.request.urlopen(bidreq)
         biddepth = json.loads(bidres.read().decode('utf8'))
 
-        askdata = '{ "method" : "book_offers", "params" : [ { "taker_gets" : { "currency" : "BTC", "issuer" : "' + config.ripple_BTC_issuer + '" }, "taker_pays" : { "currency" : "' + self.currency + '", "issuer" : "' + self.code + '" } } ] }'
+        askdata = ('{ "method" : "book_offers", '
+                   '"params" : [ { "taker_gets" : '
+                   '{ "currency" : "BTC", "issuer" : "' +
+                   config.ripple_BTC_issuer +
+                   '" }, "taker_pays" : '
+                   '{ "currency" : "' +
+                   self.currency +
+                   '", "issuer" : "' +
+                   self.code + '" } } ] }')
         askdata = askdata.encode('ascii')
-        askreq = urllib.request.Request(url, askdata,
-                                     headers={
-            "Content-Type": "application/x-www-form-urlencoded",
-            "Accept": "*/*",
-            "User-Agent": "curl/7.24.0 (x86_64-apple-darwin12.0)"})
+        askreq = urllib.request.Request(
+            url,
+            askdata,
+            headers={
+                "Content-Type": "application/x-www-form-urlencoded",
+                "Accept": "*/*",
+                "User-Agent": "curl/7.24.0 (x86_64-apple-darwin12.0)"})
         askres = urllib.request.urlopen(askreq)
         askdepth = json.loads(askres.read().decode('utf8'))
 
@@ -71,4 +92,3 @@ class Ripple(Market):
             asks.append([rate, takergets])
         asks = self.sort_and_format(asks, False)
         return {'asks': asks, 'bids': bids}
-
--- arbitrage/public_markets/rippleeur.py
+++ arbitrage/public_markets/rippleeur.py
@@ -1,6 +1,7 @@
 from ._ripple import Ripple
 import config
 
+
 class RippleEUR(Ripple):
     def __init__(self):
         super().__init__("EUR", config.ripple_EUR_issuer)
