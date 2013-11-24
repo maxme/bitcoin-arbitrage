7845ab91f02c9f2483c83f8880cbc169ae029f48
roryj78 <rory@johnstons.me.uk>
2013-11-24 20:21:44 +0000

    Update mtgox.py
    Added class PrivateMtGoxGBP():

--- arbitrage/private_markets/mtgox.py
+++ arbitrage/private_markets/mtgox.py
@@ -158,6 +158,30 @@ class PrivateMtGoxEUR(PrivateMtGox):
             return 1
         return None
 
+class PrivateMtGoxGBP(PrivateMtGox):
+    def __init__(self):
+        super().__init__()
+        self.ticker_url = {"method": "GET", "url":
+                           "https://mtgox.com/api/1/BTCGBP/public/ticker"}
+        self.buy_url = {"method": "POST", "url":
+                        "https://mtgox.com/api/1/BTCGBP/private/order/add"}
+        self.sell_url = {"method": "POST", "url":
+                         "https://mtgox.com/api/1/BTCGBP/private/order/add"}
+        self.currency = "GBP"
+
+    def get_info(self):
+        params = [("nonce", self._create_nonce())]
+        response = self._send_request(self.info_url, params)
+        if response and "result" in response and response["result"] == "success":
+            self.btc_balance = self._from_int_amount(int(
+                response["return"]["Wallets"]["BTC"]["Balance"]["value_int"]))
+            self.gbp_balance = self._from_int_price(int(
+                response["return"]["Wallets"]["GBP"]["Balance"]["value_int"]))
+            self.usd_balance = self.fc.convert(self.gbp_balance, "GBP", "USD")
+            return 1
+        return None
+
+
 class PrivateMtGoxUSD(PrivateMtGox):
     def __init__(self):
         super().__init__()
