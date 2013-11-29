b451f20f340d3be6363b4b0b54240d77f38cef0c
Maxime Biais <maxime.biais@gmail.com>
2013-11-29 09:44:44 +0100

    failback on Yahoo finance API

--- arbitrage/fiatconverter.py
+++ arbitrage/fiatconverter.py
@@ -10,6 +10,7 @@ import time
 class FiatConverter:
     __shared_state = {}
     rate_exchange_url = "http://rate-exchange.appspot.com/currency?from=%s&to=%s"
+    rate_exchange_url_yahoo = "http://download.finance.yahoo.com/d/quotes.csv?s=%s%s=X&f=sl1d1&e=.csv"
 
     def __init__(self):
         """USD is used as pivot"""
@@ -22,7 +23,7 @@ class FiatConverter:
         }
         self.update_delay = 60 * 60 # every hour
         self.last_update = 0
-        self.bank_fee = 0.007 # bank fee
+        self.bank_fee = 0.007 # FIXME: bank fee
 
     def get_currency_pair(self, code_from, code_to):
         url = self.rate_exchange_url % (code_from, code_to)
@@ -35,11 +36,21 @@ class FiatConverter:
             logging.error("Can't update fiat conversion rate: %s", url)
         return rate
 
+    def get_currency_pair_yahoo(self, code_from, code_to):
+        url = self.rate_exchange_url_yahoo % (code_from, code_to)
+        res = urllib.request.urlopen(url)
+        data = res.read().decode('utf8').split(",")[1]
+        rate = float(data) * (1.0 - self.bank_fee)
+        return rate
+
     def update_currency_pair(self, code_to):
         if code_to == "USD":
             return
         code_from = "USD"
-        rate = self.get_currency_pair(code_from, code_to)
+        try:
+            rate = self.get_currency_pair(code_from, code_to)
+        except urllib.error.HTTPError:
+            rate = self.get_currency_pair_yahoo(code_from, code_to)
         if rate:
             self.rates[code_to] = rate
 
