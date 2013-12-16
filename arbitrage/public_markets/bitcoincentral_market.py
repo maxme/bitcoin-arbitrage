8fdf2d10627f35569609663771d4769e211caae0
weidenrinde <weidenrinde_spamprotect_@gmx.de>
2013-12-16 19:11:12 +0100

    bitcoin_central URL corrected

--- arbitrage/public_markets/bitcoincentral_market.py
+++ arbitrage/public_markets/bitcoincentral_market.py
@@ -6,10 +6,19 @@ from .market import Market
 
 
 class BitcoinCentral(Market):
+    def __init__(self, **kwargs):
+        super(BitcoinCentral, self).__init__(**kwargs)
+        self.trade_fee = 0.0059     # outgoing sepa, may change in future: https://bitcoin-central.net/page/help/fees
+
+        if self.price_currency != "EUR" or self.amount_currency != "BTC":
+            raise Exception("Invalid BitcoinCentral currency pair: %s/%s" % (
+                self.amount_currency, self.price_currency
+            ))
+
     def update_depth(self):
         res = urllib.request.urlopen(
-            'https://bitcoin-central.net/api/v1/depth?currency='
-            + self.price_currency
+            'https://bitcoin-central.net/api/data/%s/depth' %
+            self.price_currency.lower()
         )
         depth = json.loads(res.read().decode('utf8'))
         self.depth = self.format_depth(depth)
