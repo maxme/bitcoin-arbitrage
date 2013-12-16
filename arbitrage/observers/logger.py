f4bebeb85ac53909293ff72b45e28158650f0538
weidenrinde <weidenrinde_spamprotect_@gmx.de>
2013-12-16 19:04:09 +0100

    In DEBUG mode, format ticker to make it more readable and print all tradechains

--- arbitrage/observers/logger.py
+++ arbitrage/observers/logger.py
@@ -5,5 +5,9 @@ from .observer import Observer
 class Logger(Observer):
     def opportunity(self, tradechains):
         best_chain = sorted(tradechains, key=lambda x: x.profit)[-1]
-        logging.info(str(best_chain))
+        if logging.root.isEnabledFor(logging.DEBUG):
+            for chain in tradechains:
+                logging.debug(str(chain))
+        else:
+            logging.info(str(best_chain))
         [h.flush() for h in logging.getLogger().handlers]
