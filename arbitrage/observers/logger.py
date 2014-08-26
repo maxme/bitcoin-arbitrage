9f89408fccc9ecd49da0cebc8b6fb885b8aa71e9
Maxime Biais <maxime.biais@gmail.com>
2014-08-26 20:11:15 +0200

    add a new DetailedLogger observer

--- arbitrage/observers/logger.py
+++ arbitrage/observers/logger.py
@@ -5,4 +5,5 @@ from .observer import Observer
 class Logger(Observer):
     def opportunity(self, profit, volume, buyprice, kask, sellprice, kbid, perc,
                     weighted_buyprice, weighted_sellprice):
-        logging.info("profit: %f USD with volume: %f BTC - buy at %.4f (%s) sell at %.4f (%s) ~%.2f%%" % (profit, volume, buyprice, kask, sellprice, kbid, perc))
+        logging.info("profit: %f USD with volume: %f BTC - buy from %s sell to %s ~%.2f%%" \
+        	% (profit, volume, kask, kbid, perc))
