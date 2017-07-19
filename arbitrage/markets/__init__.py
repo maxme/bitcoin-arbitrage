aa9cdcfac554d50ef0422ad2c3908a6000286610
Eric Scheier <eric@scheier.org>
2017-07-19 07:48:08 -0400

    change to all CAPS exchange_names in message & remove campbtc due to bugs

--- arbitrage/markets/__init__.py
+++ arbitrage/markets/__init__.py
@@ -3,7 +3,7 @@ from arbitrage.markets import bitstampusd
 from arbitrage.markets import btcccny
 from arbitrage.markets import btceeur
 from arbitrage.markets import btceusd
-from arbitrage.markets import campbxusd
+# from arbitrage.markets import campbxusd
 from arbitrage.markets import coinbaseusd
 from arbitrage.markets import geminiusd
 from arbitrage.markets import krakeneur
