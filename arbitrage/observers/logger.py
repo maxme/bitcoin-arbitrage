746c7f55f20dc1d71bec09a5098d8832c74c1b39
flufy3d <flufy3d@gmail.com>
2018-02-20 02:59:03 +0100

    log small adjustment

--- arbitrage/observers/logger.py
+++ arbitrage/observers/logger.py
@@ -6,6 +6,7 @@ import time
 class Logger(Observer):
     def __init__(self):
         self.last_log_time = time.time()
+        self.prev_perc = 0
     def opportunity(self, profit, volume, buyprice, kask, sellprice, kbid, perc,
                     weighted_buyprice, weighted_sellprice):
 
@@ -13,7 +14,8 @@ class Logger(Observer):
         pair1_name = str.upper(pair_names[0])
         pair2_name = str.upper(pair_names[1])
 
-        if time.time() - self.last_log_time > 1:
+        if time.time() - self.last_log_time > 1 or perc > self.prev_perc:
             logging.info("profit: %f %s with volume: %f %s - buy from %s sell to %s ~%.2f%%" \
             	% (profit, pair2_name, volume, pair1_name, kask, kbid, perc))
             self.last_log_time = time.time()
+            self.prev_perc = perc
