1ff0995a7bc582a258e128bc1d932d702b4b7e43
Maxime Biais <maxime.biais@gmail.com>
2019-04-04 22:34:12 +0200

    Use European bank XML endpoint as a source for fiat convertion

--- arbitrage/fiatconverter.py
+++ arbitrage/fiatconverter.py
@@ -1,15 +1,32 @@
-# Copyright (C) 2013, Maxime Biais <maxime@biais.org>
+# Copyright (C) 2013-2019, Maxime Biais <maxime@bia.is>
 
 import urllib.request
 import sys
-import json
+import xml.sax
 import logging
 import time
 
 
+class XmlHandler(xml.sax.ContentHandler):
+    def __init__(self):
+        self.currentTag = ""
+        self.rates = {}
+
+    def startElement(self, tag, attributes):
+        if "currency" in attributes and "rate" in attributes:
+            self.rates[attributes["currency"]] = float(attributes["rate"])
+
+    def endElement(self, tag):
+        pass
+
+    def characters(self, content):
+        pass
+
+
 class FiatConverter:
     __shared_state = {}
-    rate_exchange_url = "https://free.currencyconverterapi.com/api/v5/convert?q=%s_%s&compact=ultra"
+    # Europa.eu is the official european central bank
+    rate_exchange_url = "https://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml"
 
     def __init__(self):
         """USD is used as pivot"""
@@ -20,36 +37,30 @@ class FiatConverter:
             "CNY": 6.15,
             "SEK": 6.6,
         }
-        self.update_delay = 60 * 60 # every hour
+        self.update_delay = 60 * 60 * 6  # every 6 hours
         self.last_update = 0
-        self.bank_fee = 0.007 # FIXME: bank fee
+        self.bank_fee = 0.007  # TODO: bank fee
+        self.handler = XmlHandler()
 
-    def get_currency_pair(self, code_from, code_to):
-        url = self.rate_exchange_url % (code_from, code_to)
+    def make_USD_pivot(self, rates):
+        USDEUR = 1.0 / rates['USD']
+        rates['EUR'] = USDEUR
+        for k in rates:
+            rates[k] = rates[k] * USDEUR
+        return rates
+
+    def update_pairs(self):
+        url = self.rate_exchange_url
         res = urllib.request.urlopen(url)
-        data = json.loads(res.read().decode('utf8'))
-        rate = 0
-        try:
-            rate = float(data[str.format("{}_{}", code_from, code_to)] * (1.0 - self.bank_fee))
-        except:
-            logging.error("Can't update fiat conversion rate: %s", url)
-        return rate
-
-    def update_currency_pair(self, code_to):
-        if code_to == "USD":
-            return
-        code_from = "USD"
-        rate = self.get_currency_pair(code_from, code_to)
-        if rate:
-            self.rates[code_to] = rate
+        xml.sax.parseString(res.read().decode('utf8'), self.handler)
+        self.rates = self.make_USD_pivot(self.handler.rates)
 
     def update(self):
         timediff = time.time() - self.last_update
         if timediff < self.update_delay:
             return
         self.last_update = time.time()
-        for currency in self.rates:
-            self.update_currency_pair(currency)
+        self.update_pairs()
 
     def convert(self, price, code_from, code_to):
         self.update()
@@ -62,4 +73,5 @@ if __name__ == "__main__":
     fc = FiatConverter()
     print(fc.convert(12., "USD", "EUR"))
     print(fc.convert(12., "EUR", "USD"))
+    print(fc.convert(fc.convert(12., "EUR", "USD"), "USD", "EUR"))
     print(fc.rates)
