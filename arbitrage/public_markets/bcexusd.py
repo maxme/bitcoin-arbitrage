8a0e95e336caeca9e3fab92b4333c3c8b6a028f2
james-bc <james@blockchain.com>
2020-05-03 16:28:59 +0100

    uncomment main

--- arbitrage/public_markets/bcexusd.py
+++ arbitrage/public_markets/bcexusd.py
@@ -6,9 +6,9 @@ class BcexUSD(Bcex):
         super().__init__("USD", "BTC-USD")
 
 
-# if __name__ == "__main__":
-#     market = BcexUSD()
-#     while True:
-#         sleep(2)
-#         market.update_depth()
-#         print(market.get_ticker())
+if __name__ == "__main__":
+    market = BcexUSD()
+    while True:
+        sleep(2)
+        market.update_depth()
+        print(market.get_ticker())
